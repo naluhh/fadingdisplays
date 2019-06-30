722585d67ada02056d9f05a1b8c9d767e61d8dbf
charly <charly@zen.ly>
2019-06-30 16:33:06 +0200

    add xcode proj

---
+++ iosapp/fadingdisplay/fadingdisplayUITests/fadingdisplayUITests.swift
@@ -0,0 +1,34 @@
+//
+//  fadingdisplayUITests.swift
+//  fadingdisplayUITests
+//
+//  Created by Charly Delaroche on 6/12/19.
+//  Copyright © 2019 Charly Delaroche. All rights reserved.
+//
+
+import XCTest
+
+class fadingdisplayUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
